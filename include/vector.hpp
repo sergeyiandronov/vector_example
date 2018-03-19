7a1aad5b094af96e1d037dcaf5ae06d4f27f2d84
sergeyiandronov <sergeyiandronov@gmail.com>
2018-03-19 22:06:37 +0300

    vector0.0.2

--- include/vector.hpp
+++ include/vector.hpp
@@ -1,12 +1,14 @@
 #include <iostream>
-
+template <typename T>
 class vector_t
 {
 private:
-	int * elements_;
+	T * elements_;
 	std::size_t size_;
 	std::size_t capacity_;
+    void at(std::size_t index) const;
 public:
+  
 	vector_t();
 	vector_t(vector_t const & other);
 	vector_t & operator =(vector_t const & other);
@@ -14,14 +16,169 @@ public:
 
 	std::size_t size() const;
 	std::size_t capacity() const;
-
-	void push_back(int value);
+    
+	void push_back(T value);
 	void pop_back();
 
-	int & operator [](std::size_t index);
-	int operator [](std::size_t index) const;
-    friend bool operator !=(vector_t const & lhs, vector_t const & rhs);
+	T & operator [](std::size_t index);
+	T operator [](std::size_t index) const;
+    
 	bool operator ==(vector_t const & other) const;
 };
+template<typename T>
+bool operator !=(vector_t<T> const & lhs, vector_t<T> const & rhs);
+#include <algorithm>
+#include <cassert>
+template <typename T>
+void vector_t<T>::at(std::size_t index) const
+{
+    if(index>=capacity_){
+        throw "index out of range"; 
+    }
+}
+template <typename T>
+vector_t<T>::vector_t()
+{
+  elements_=nullptr;
+  size_=0;
+  capacity_=0;
+}
+template <typename T>
+vector_t<T>::vector_t(vector_t const & other)
+{
+  elements_=new T[other.capacity()];
+  for(int i=0;i<other.size();i++){
+    elements_[i]=other.elements_[i];
+  }
+  size_=other.size();
+  capacity_=other.capacity();
+}
+template <typename T>
+vector_t<T> & vector_t<T>::operator =(vector_t const & other)
+{   if(this!=&other){
+           if(elements_!=nullptr){
+                  delete[] elements_; 
+           }
+           elements_=new T[other.capacity()];
+           for(int i=0;i<other.size();i++){
+               elements_[i]=other.elements_[i];
+           } 
+           size_=other.size();
+           capacity_=other.capacity();
+    }
 
-bool operator !=(vector_t const & lhs, vector_t const & rhs);
+   return *this;
+
+}
+template <typename T>
+bool vector_t<T>::operator ==(vector_t const & other) const
+{
+  bool result=true;
+  if(size_!=other.size()){
+    return false;
+  }
+  for(int i=0;i<other.size();i++){
+    if(elements_[i]!=other.elements_[i]){
+      result=false;
+      return result;
+    }
+  }
+
+  return result;
+}
+template <typename T>
+vector_t<T>::~vector_t()
+{
+  if(elements_!=nullptr){
+        delete[] elements_; 
+    }
+}
+template <typename T>
+std::size_t vector_t<T>::size() const
+{
+    return size_;
+}
+template <typename T>
+std::size_t vector_t<T>::capacity() const
+{
+    return capacity_;
+}
+template <typename T>
+void vector_t<T>::push_back(T value)
+{
+     size_+=1;
+     if(size_>capacity_){
+      if(capacity_==0){
+        capacity_=1;
+      }else{
+          capacity_=2*capacity_;
+      }
+      
+      T *elements=new T[capacity_];
+      for(int i=0;i<size_-1;i++){
+        elements[i]=elements_[i];
+      }
+      elements[size_-1]=value;
+      if(elements_!=nullptr){
+            delete[] elements_; 
+        }
+        elements_=elements;
+
+     }
+     else{
+      elements_[size_-1]=value;
+     }
+
+}
+template <typename T>
+void vector_t<T>::pop_back()
+{   
+  if (size_==0){
+    return;
+  }
+  size_-=1;
+  
+  if(size_<=(capacity_/4)){
+      capacity_=capacity_/2;
+      T *elements=new T[capacity_];
+      for(int i=0;i<size_;i++){
+        elements[i]=elements_[i];
+      }
+      
+      if(elements_!=nullptr){
+            delete[] elements_; 
+        }
+      elements_=elements;
+
+     }
+     
+}
+template <typename T>
+T & vector_t<T>::operator [](std::size_t index)
+{
+  at(index);
+  return elements_[index];
+}
+template <typename T>
+T vector_t<T>::operator [](std::size_t index) const
+{
+  at(index);
+  return elements_[index];
+}
+template <typename T>
+bool operator !=(vector_t<T> const & lhs, vector_t<T> const & rhs)
+{   
+  bool result=false;
+  if(rhs.size()!=lhs.size()){
+    return true;
+  }
+  
+  if(!(lhs==rhs)){
+      result=true;
+      return result;
+  }
+  
+
+  return result;
+  
+}
--- tests/vector.cpp
+++ tests/vector.cpp
@@ -5,24 +5,24 @@
 
 TEST_CASE("creating vector")
 {
-	vector_t vector;
+	vector_t<int> vector;
 	REQUIRE( vector.size() == 0 );
 	REQUIRE( vector.capacity() == 0 );
 }
 
 TEST_CASE("copying vector")
 {
-	vector_t vector;
+	vector_t<int> vector;
 	vector.push_back(1);
 
-	vector_t copy(vector);
+	vector_t<int> copy(vector);
 	REQUIRE( copy == vector );
 }
 
 TEST_CASE("assigning vector")
 {
-	vector_t vector1;
-	vector_t vector2;
+	vector_t<int> vector1;
+	vector_t<int> vector2;
 
 	vector1.push_back(1);
 	vector2.push_back(2);
@@ -33,8 +33,8 @@ TEST_CASE("assigning vector")
 
 TEST_CASE("equaling vector")
 {
-	vector_t vector1;
-	vector_t vector2;
+	vector_t<int> vector1;
+	vector_t<int> vector2;
 
 	vector1.push_back(1);
 	vector2.push_back(1);
@@ -47,19 +47,19 @@ TEST_CASE("equaling vector")
 
 TEST_CASE("indexing vector")
 {
-	vector_t vector;
+	vector_t<int> vector;
 
 	vector.push_back(1);
-
-	REQUIRE( vector[0] == 1 );
-
-	vector_t const copy(vector);
+    REQUIRE_NOTHROW( vector[0] == 1 );
+    REQUIRE( vector[0] == 1 );
+	vector_t<int> const copy(vector);
 	REQUIRE( copy[0] == 1 );
+    REQUIRE_NOTHROW( vector[0] == 1 );
 }
 
 TEST_CASE("pushing elements")
 {
-	vector_t vector;
+	vector_t<int>  vector;
 
 	vector.push_back(1);
 	REQUIRE( vector.size() == 1 );
@@ -84,7 +84,7 @@ TEST_CASE("pushing elements")
 
 TEST_CASE("poping elements")
 {
-	vector_t vector;
+	vector_t<int> vector;
 
 	vector.push_back(1);
 	vector.push_back(2);
@@ -116,5 +116,105 @@ TEST_CASE("poping elements")
 	vector.pop_back();
 	REQUIRE( vector.size() == 0 );
  	REQUIRE( vector.capacity() == 1 );
-    vector.push_back(5);
+}
+TEST_CASE("type_testing"){
+  
+  
+   
+    vector_t<int> vector;
+    vector_t<float> vector1;
+    vector_t<double> vector2;
+    vector.push_back(1);
+	vector.push_back(2);
+	vector.push_back(3);
+	vector.push_back(4);
+	vector.push_back(5);
+	vector.push_back(6);
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 5 );
+	REQUIRE( vector.capacity() == 8 );
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 4 );
+	REQUIRE( vector.capacity() == 8 );
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 3 );
+	REQUIRE( vector.capacity() == 8 );
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 2 );
+	REQUIRE( vector.capacity() == 4 );
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 1 );
+	REQUIRE( vector.capacity() == 2 );
+
+	vector.pop_back();
+	REQUIRE( vector.size() == 0 );
+ 	REQUIRE( vector.capacity() == 1 ); 
+    vector1.push_back(1);
+	vector1.push_back(2);
+	vector1.push_back(3);
+	vector1.push_back(4);
+	vector1.push_back(5);
+	vector1.push_back(6);
+
+	vector1.pop_back();
+	REQUIRE( vector1.size() == 5 );
+	REQUIRE( vector1.capacity() == 8 );
+
+	vector1.pop_back();
+	REQUIRE( vector1.size() == 4 );
+	REQUIRE( vector1.capacity() == 8 );
+
+	vector1.pop_back();
+	REQUIRE( vector1.size() == 3 );
+	REQUIRE( vector1.capacity() == 8 );
+
+	vector1.pop_back();
+	REQUIRE( vector1.size() == 2 );
+	REQUIRE( vector1.capacity() == 4 );
+
+	vector1.pop_back();
+	REQUIRE( vector1.size() == 1 );
+	REQUIRE( vector1.capacity() == 2 );
+
+	vector1.pop_back();
+	REQUIRE( vector1.size() == 0 );
+ 	REQUIRE( vector1.capacity() == 1 );
+vector2.push_back(1);
+	vector2.push_back(2);
+	vector2.push_back(3.4435647);
+	vector2.push_back(4);
+	vector2.push_back(5);
+	vector2.push_back(6);
+
+	vector2.pop_back();
+	REQUIRE( vector2.size() == 5 );
+	REQUIRE( vector2.capacity() == 8 );
+
+	vector2.pop_back();
+	REQUIRE( vector2.size() == 4 );
+	REQUIRE( vector2.capacity() == 8 );
+
+	vector2.pop_back();
+	REQUIRE( vector2.size() == 3 );
+	REQUIRE( vector2.capacity() == 8 );
+
+	vector2.pop_back();
+	REQUIRE( vector2.size() == 2 );
+	REQUIRE( vector2.capacity() == 4 );
+
+	vector2.pop_back();
+	REQUIRE( vector2.size() == 1 );
+	REQUIRE( vector2.capacity() == 2 );
+
+	vector2.pop_back();
+	REQUIRE( vector2.size() == 0 );
+ 	REQUIRE( vector2.capacity() == 1 ); 
+  
+   
+
 }
